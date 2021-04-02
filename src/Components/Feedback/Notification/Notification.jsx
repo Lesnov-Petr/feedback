bd35af350c5874e024cc38a1cf374e445cad838e
Lesnov Petr <lesnov,p@gmail.com>
2021-04-03 00:00:00 +0300

    Update Notification.jsx

--- src/Components/Feedback/Notification/Notification.jsx
+++ src/Components/Feedback/Notification/Notification.jsx
@@ -1,6 +1,12 @@
+import PropTypes from "prop-types";
 import "./index.scss";
 
 const Notification = ({ message }) => (
   <p className="message">No feedback given &#128530;</p>
 );
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
 export default Notification;
