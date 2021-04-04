428bfb3dbfcbfec11f669cd5d943e910d4288030
Lesnov Petr <lesnov,p@gmail.com>
2021-04-04 22:50:31 +0300

    правки

--- src/Components/Feedback/Statistics/Statistics.jsx
+++ src/Components/Feedback/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import Notification from "../Notification/Notification";
+import Notification from "../Notification";
 import "./index.scss";
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
